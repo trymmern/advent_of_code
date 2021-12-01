d2c3b4d78a01eceeb68db1914d6b46746606a242
Trym Røsand Todalshaug <trymrt@gmail.com>
2021-12-01 13:49:40 +0100

    Initial commit (contains first day solutions)

---
+++ 01/Helpers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode._01
+{
+    public static class Helpers
+    {
+        public static List<string> ReadFile()
+        {
+            return File.ReadAllLines("C:\\dev\\AdventOfCode\\01\\input.txt").ToList();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using AdventOfCode._01;
+
+namespace AdventOfCode
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine($"Result: {_01_1.GetIncreasedDepthCount()} \n\n");
+            Console.WriteLine($"Result: {_01_2.GetIncreasedDepthValueAmountX3()} \n\n");
+        }
+    }
+}
