6837c3c3bfbb33c01b752d65f32d389e48aa0007
Trym Røsand Todalshaug <trym.rosand.todalshaug@nhn.no>
2022-12-06 11:47:29 +0100

    remove prints

--- 2022/06/0602.py
+++ 2022/06/0602.py
@@ -8,8 +8,6 @@ for x in f:
     for i in range(0, len(x)):
         queue.pop();
         queue.appendleft(x[i]);
-        print(queue)
-        print(len(set(queue)), len(queue))
         if (len(queue) == len(set(queue)) and '' not in queue):
             print(i + 1)
             sys.exit()
