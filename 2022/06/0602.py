5394f482dfceeead40ffcad6472d0a49901ef5b7
Trym Røsand Todalshaug <trym.rosand.todalshaug@nhn.no>
2022-12-06 10:23:04 +0100

    day 6

---
+++ 2022/06/0602.py
@@ -0,0 +1,15 @@
+from collections import deque
+import sys
+
+f = open("input.txt", "r")
+
+queue = deque([""]*14)
+for x in f:
+    for i in range(0, len(x)):
+        queue.pop();
+        queue.appendleft(x[i]);
+        print(queue)
+        print(len(set(queue)), len(queue))
+        if (len(queue) == len(set(queue)) and '' not in queue):
+            print(i + 1)
+            sys.exit()
